c358ffd6cbfaaa8d53dfb5c4153f381878c7b651
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2024-03-16 19:18:13 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,8 @@ export default defineConfig({
   test: {
     environment: "jsdom",
   },
-  server: {
-    host: true,
-    port: 5173,
-  }
+  // server: {
+  //   host: true,
+  //   port: 5173,
+  // }
 });
