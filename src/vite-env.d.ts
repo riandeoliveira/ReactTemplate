5b52a511e1a32fa4b6aac4318ec41281fa20db89
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2024-03-16 17:39:24 -0300

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,20 +1,10 @@
 /// <reference types="vite-plugin-svgr/client" />
 /// <reference types="vite/client" />
 
-import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     environment: "jsdom",
-  },
-  plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-    host: true,
-    strictPort: true,
-    port: 5173,
   }
 });
